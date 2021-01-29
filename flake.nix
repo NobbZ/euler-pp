9667078b5e33246282387d703da225a25ef88200
Norbert Melzer <timmelzer@gmail.com>
2021-01-29 08:23:49 +0100

    add doxygen and graphviz

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       devShell.x86_64-linux = pkgs.mkShell {
         pname = "aoc-c-shell";
 
-        buildInputs = [ pkgs.gnumake pkgs.bear ];
+        buildInputs = [ pkgs.gnumake pkgs.bear pkgs.doxygen pkgs.graphviz ];
       };
     };
 }
