b8c44d1b7f3d2bcf004fc4c59b024f879b6eabd8
Norbert Melzer <timmelzer@gmail.com>
2025-04-21 15:05:49 +0200

    make the CLI print the URLs to the problems

--- include/euler.hpp
+++ include/euler.hpp
@@ -4,12 +4,23 @@
 #include <string>
 
 namespace euler {
+
+/** A virtual class to be used as a blueprint for actual euler problems
+ *
+ */
 class euler {
 public:
+  /** This field shall return the actual ID of the problem. */
   virtual int16_t id() = 0;
+  /** This field shall return the actual title of the problem. */
   virtual std::string caption() = 0;
+  /** This field shall return a longer description of the problem. */
   virtual std::string description() = 0;
 
+  /** Runs the actual problem solution and returns the result. */
   virtual uint64_t run() = 0;
+
+  /** Returns the URL of the problem description on project eurler. */
+  std::string url();
 };
 } // namespace euler
---
+++ src/euler.cpp
@@ -0,0 +1,13 @@
+#include <sstream>
+
+#include "euler.hpp"
+
+const std::string BASE_URL = "https://projecteuler.net/";
+
+std::string euler::euler::url() {
+  std::ostringstream oss;
+
+  oss << BASE_URL << "format=" << this->id();
+
+  return oss.str();
+};
