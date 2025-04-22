f066db3f03dc5656e0024cc875d9c1907481f226
Norbert Melzer <timmelzer@gmail.com>
2025-04-22 22:51:12 +0200

    split build into lib and executable

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       devShell.x86_64-linux = pkgs.mkShell {
         pname = "aoc-c-shell";
 
-        packages = [ pkgs.gnumake pkgs.bear pkgs.doxygen pkgs.graphviz pkgs.meson pkgs.mesonlsp pkgs.ninja pkgs.clang-tools ];
+        packages = [ pkgs.gnumake pkgs.cmake pkgs.bear pkgs.doxygen pkgs.graphviz pkgs.meson pkgs.mesonlsp pkgs.ninja pkgs.clang-tools pkgs.catch2_3 pkgs.pkg-config ];
       };
     };
 }
