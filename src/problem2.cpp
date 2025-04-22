ab76603cc08fa44eecde6cdebb440e6a41ad04e8
Norbert Melzer <timmelzer@gmail.com>
2025-04-22 21:15:34 +0200

    use the lazy fobs in problem 2

--- src/problem2.cpp
+++ src/problem2.cpp
@@ -1,23 +1,24 @@
+#include <cstdint>
+#include <numeric>
+#include <ranges>
+
+#include "numbers.hpp"
+
 #include "problem2.hpp"
 
-const uint64_t UPPER_BOUND = 4000000; 
+const uint64_t UPPER_BOUND = 4000000;
 
 int16_t euler::problem2::id() const { return 2; }
 
 std::string euler::problem2::caption() { return "Even Fibonacci numbers"; }
 
 uint64_t euler::problem2::run() {
-  uint64_t sum{0};
-  uint64_t i{1}, j{1};
-  uint64_t fib { i + j };
+  auto even_fibs =
+      numbers::fibonacci_sequence |
+      std::views::filter([](uint64_t n) noexcept { return n % 2 == 0; }) |
+      std::views::take_while([](uint64_t n) noexcept {
+        return n < UPPER_BOUND;
+      }) | std::views::common;
 
-  while (fib < UPPER_BOUND) {
-    if (fib % 2 == 0) sum += fib;
-
-    i = j;
-    j = fib;
-    fib = i + j;
-  }
-
-  return sum;
+  return std::accumulate(even_fibs.begin(), even_fibs.end(), UINT64_C(0));
 }
