5e9e9a4271c7084619237111bd9fa38619aaf2eb
Norbert Melzer <timmelzer@gmail.com>
2025-04-22 21:16:26 +0200

    some constexpr'ing in problem 6

--- src/problem6.cpp
+++ src/problem6.cpp
@@ -3,19 +3,23 @@
 
 #include "problem6.hpp"
 
-uint64_t square_of_sums(uint64_t) __attribute__((const));
-uint64_t sum_of_squares(uint64_t) __attribute__((const));
+constexpr uint64_t square_of_sums(uint64_t) __attribute__((const));
+constexpr uint64_t sum_of_squares(uint64_t) __attribute__((const));
 
 int16_t euler::problem6::id() const { return 6; }
 
 std::string euler::problem6::caption() { return "Sum Square Difference"; }
 
-uint64_t euler::problem6::run() { return square_of_sums(100) - sum_of_squares(100); }
+uint64_t euler::problem6::run() {
+  return square_of_sums(100) - sum_of_squares(100);
+}
 
-uint64_t square_of_sums(uint64_t n) {
+constexpr uint64_t square_of_sums(uint64_t n) {
   uint64_t sum{(n * n + n) / 2};
 
   return sum * sum;
 }
 
-uint64_t sum_of_squares(uint64_t n) { return (n * (n + 1) * (2 * n + 1)) / 6; }
+constexpr uint64_t sum_of_squares(uint64_t n) {
+  return (n * (n + 1) * (2 * n + 1)) / 6;
+}
