bcb697b87a579f527b78518d09679298e01436dd
Norbert Melzer <timmelzer@gmail.com>
2025-04-23 23:55:33 +0200

    realign output of main

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main() {
   std::for_each(problems.cbegin(), problems.cend(), [](euler::euler *p) {
     std::cout << "Problem " << std::setfill(' ') << std::setw(3) << p->id()
               << ": " << std::setw(30) << std::quoted(p->caption()) << " "
-              << std::setw(10) << p->run() << std::endl;
+              << std::setw(11) << p->run() << std::endl;
 
-    std::cout << std::setw(54) << p->url() << std::endl;
+    std::cout << std::setw(55) << p->url() << std::endl;
   });
 }
