f58084908fb4670a849e8e3878aa047f8f002c0b
Norbert Melzer <timmelzer@gmail.com>
2025-04-26 21:49:53 +0200

    clean up problem 7

--- src/problem7.cpp
+++ src/problem7.cpp
@@ -9,8 +9,7 @@
 REGISTER_EULER_PROBLEM(euler::problem7, 7, "10 001st Prime")
 
 uint64_t euler::problem7::run() {
-  // return *std::next(numbers::primes().begin(), 10000);
-  auto prime = numbers::primes() | std::views::take(10001) | std::views::drop(10000);
+  auto prime = numbers::primes() | std::views::drop(10000);
 
   auto it = prime.begin();
   return *it;
